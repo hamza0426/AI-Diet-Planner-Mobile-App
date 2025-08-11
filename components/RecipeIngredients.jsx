965baf330d72230ae2fc727d3d61d4fcce18a31c
Fahadkhan1503 <fahadkhan1503@gmail.com>
2025-08-11 15:41:27 +0500

    minor fix

--- components/RecipeIngredients.jsx
+++ components/RecipeIngredients.jsx
@@ -1,7 +1,7 @@
 import { Text, View } from 'react-native'
 import React from 'react'
 
-export default function RecipeIngredients({recipeDetai}) {
+export default function RecipeIngredients({recipeDetail}) {
     return (
       <View>
             <Text>RecipeIngredients</Text>
