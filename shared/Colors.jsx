b01ed23735e239ff15026478649d8ab6471f0334
hamza0426 <muhammadhamzaowais5@gmail.com>
2025-08-27 07:39:08 +0500

    Recipe card Complete

--- shared/Colors.jsx
+++ shared/Colors.jsx
@@ -1,11 +1,11 @@
 export default {
-    WHITE: '#fff',
-    PRIMARY: '#8837ff',
-    Gray: 'gray',
-    BLUE: '#4287f5',
-    PINK: '#f542b3',
-    SECONDARY: '#fbf5ff',
-    green: '#e5f6df',
-    Green: '#6cc070'
-    
-}
+  WHITE: "#fff",
+  PRIMARY: "#8837ff",
+  Gray: "gray",
+  BLUE: "#4287f5",
+  PINK: "#f542b3",
+  SECONDARY: "#fbf5ff",
+  green: "#e5f6df",
+  Green: "#6cc070",
+  RED: "red",
+};
